6a4f5e31575f4268884e61c3b98f83de3014d9c7
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-11-05 09:20:33 -0200

    Criação de método setData em Manager.

--- prj_ghub_test/Manager.cs
+++ prj_ghub_test/Manager.cs
@@ -12,5 +12,10 @@ namespace prj_ghub_test
         public string endereco { get; private set; }
         public string cidade { get; private set; }
         public string telefone { get; private set; }
+
+        public void setData(IRegistrador registrador)
+        {
+            registrador.setInfo(this.nome, this.endereco, this.cidade, this.telefone);
+        }
     }
 }
