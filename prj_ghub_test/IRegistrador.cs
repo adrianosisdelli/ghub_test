e8f7f1462bf730384f21f4e780ba9ff3d84f8023
Adriano Sisdelli <adrianosisdelli@gmail.com>
2018-11-05 09:15:27 -0200

    Criação de interface IRegistrador.

---
+++ prj_ghub_test/IRegistrador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj_ghub_test
+{
+    public interface IRegistrador
+    {
+        void setInfo(string nome, string endereco, string cidade, string telefone);
+    }
+}
